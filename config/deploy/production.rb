a4660559ea3d33a37294dc8e9faaa8a02a2d544b
Landers9 <franckaigba4@gmail.com>
2021-09-12 19:23:39 +0100

    deployement

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-serveur'44.198.114.250', utilisateur:'app', rôles:% w {app db web}
-set: ssh_options, keys: '~/.ssh/id_rsa'
+server '44.198.114.250', user:'app', roles: %w{app db web}
+set :ssh_options, keys: '~/.ssh/id_rsa'
